25958ccc01bbd729806ca2dc42513483a1424f8e
vsavo0930 <vladislav.savostin@cbsinteractive.com>
2020-08-27 17:07:53 -0700

    Vlad Homework3

--- tests/homework3.py
+++ tests/homework3.py
@@ -1,8 +1,41 @@
+import time
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+from selenium import webdriver
+
+class Login(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome("../browsers/chromedriver")
+        self.browser.get('http://hrm-online.portnov.com/')
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+
+    def test_homework3(self):
+        browser = self.browser
+
+        browser.find_element_by_id("txtUsername").send_keys("admin")
+        browser.find_element_by_id("txtPassword").send_keys("password")
+        browser.find_element_by_id("btnLogin").click()
+        time.sleep(3)
+        browser.find_element_by_id("empsearch_supervisor_name").send_keys("Bob Boss")
+        browser.find_element_by_id("searchBtn").click()
+        browser.find_element_by_xpath("//*[@id='resultTable']/tbody/tr[2]/td[3]/a").click()
+
+        try:
+         browser.find_element_by_id("tblAttachments").is_displayed()
+        except NoSuchElementException:
+            print("No resume is attached")
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
