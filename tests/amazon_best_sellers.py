dceca2265e5d05bd3db59d4d855238a5ac5ab7c9
Ellie Yampolskaya <Ellie.Yampolskaya@gmail.com>
2020-08-29 11:19:21 -0700

    End of session 5

--- tests/amazon_best_sellers.py
+++ tests/amazon_best_sellers.py
@@ -1,7 +1,10 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class AmazonBestSellerExample(unittest.TestCase):
@@ -15,6 +18,7 @@ class AmazonBestSellerExample(unittest.TestCase):
 
     def test_find_headphones(self):
         browser = self.browser
+        wait = WebDriverWait(browser, 5)
         browser.find_element_by_id("twotabsearchtextbox").send_keys("headphones" + Keys.ENTER)
         # # OR
         # browser.find_element_by_id("twotabsearchtextbox").send_keys("headphones")
@@ -28,8 +32,22 @@ class AmazonBestSellerExample(unittest.TestCase):
 
             # '//span[@class="a-badge-label"][@data-a-badge-color="sx-orange"]//span[@class="a-badge-text"][@data-a-badge-color="sx-cloud"]'
 
-        # little cheat
-        best_sellers_list[0].find_element_by_xpath("./ancestor::div[@class='a-section a-spacing-medium']/div[2]//a").send_keys(Keys.COMMAND + Keys.ENTER)
+        for best_seller_banner in best_sellers_list:
+            # little cheat
+            best_seller_banner.find_element_by_xpath(
+                "./ancestor::div[@class='a-section a-spacing-medium']/div[2]//a"
+            ).send_keys(Keys.COMMAND + Keys.ENTER)
+
+        wait.until(expected_conditions.number_of_windows_to_be(len(best_sellers_list) + 1))
+
+        handles = browser.window_handles
+        for j in range(1, len(handles)):
+            browser.switch_to.window(handles[j])
+
+            wait.until(expected_conditions.presence_of_element_located((By.ID, "add-to-cart-button"))).click()
+            # browser.find_element_by_id("add-to-cart-button")
+            print('Done')
+
 
         pass
 
